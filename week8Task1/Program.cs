3575200af07012b351487e20eda05e492d562669
anastasiiaskub <anastasiia.skub.25@kse.org.ua>
2026-03-28 14:52:48 +0200

    done

--- week8Task1/Program.cs
+++ week8Task1/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Letter let1 = new Letter("267", 4);
+        Letter let2 = new Letter("123", 20);
+
+        Parcel parcel1 = new Parcel("345", 45, "20X10X30");
+        Parcel parcel2 = new Parcel("678", 5, "10X5X20");
+
+        DeliveryItem item1 = let1;
+        DeliveryItem item2 = parcel2;
+        
+        item1.PrintInfo();
+        item2.PrintInfo();
+
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+        myCargo.AddItem(let1);
+        myCargo.AddItem(let2);
+        myCargo.AddItem(parcel1);
+        myCargo.AddItem(parcel2);
+        myCargo.AddItem(item1);
+        myCargo.AddItem(item2);
+
+        Console.WriteLine($"total cost: {myCargo.GetTotalCost()}");
     }
 }
